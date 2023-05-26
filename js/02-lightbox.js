65c5cfc98e07259da3c21147e7e01d2f5349b45a
yurii-corssa <yurii.corssa@gmail.com>
2023-05-26 17:51:39 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+const createGalleryMarkup = (images) =>
+  images
+    .map(
+      ({ original, preview, description }) => `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`
+    )
+    .join("");
+
+galleryRef.innerHTML = createGalleryMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
